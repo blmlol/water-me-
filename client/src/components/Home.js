78adb2b4b8aec0d4a946ace3e9f72bc7453b0987
Davey Wong <notwdjw@gmail.com>
2020-08-14 15:04:59 -0700

    Added some home page buttons and added CSS formatting

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import {Container, Nav, Button, Image} from 'react-bootstrap';
+import { Container, Navbar, Nav, Button, Image } from 'react-bootstrap';
 import waterplant from '../images/plant.png';
-function Home (props) {
 
 
-    return(
+function Home(props) {
+    const style = {
+        height: '150.25',
+        width: '149px'
+    }
+
+    const water = {
+        color: '#00BFFF'
+
+    }
+
+    const bg = {
+        backgroundColor: '#FFB6C1',
+        backgroundSize: 'cover'
+      
+    }
+
+    const buttext = {
+        color: '#4B0082'
+    }
+
+    return (
 
 
         <>
-        <Container> 
+            <Container style={bg} className='text-center' fluid>
 
-            <Image src= {waterplant} rounded/>
-        </Container>
+                <Button style={buttext} variant='outline-primary' className='float-left m-2 font-weight-bold'> Login</Button>
+                <Button style={buttext} variant='outline-primary' className='float-left m-2 font-weight-bold'> Signup</Button>
+
+                <h1 style={water} className='font-weight-bold fixed-top'>
+                    Water Me
+              </h1>
+
+                <br></br>
+                <br></br>
+                <br></br>
+                <Image style={style} src={waterplant} alt='plant watering by Icon Island from the Noun Project' rounded />
+
+
+            </Container>
         </>
     )
 }
